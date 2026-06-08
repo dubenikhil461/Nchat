9eb5918afd2492fa085267f2e3fcde830e800df2
dubenikhil461 <nikhildubey461@gmail.com>
2026-06-08 23:41:48 +0530

    Add message schema and migration scripts; remove old auth schema

---
+++ drizzle/0000_demonic_stellaris.sql
@@ -0,0 +1,25 @@
+CREATE TABLE `auth` (
+	`id` varchar(6) NOT NULL,
+	`name` varchar(30) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`password` varchar(255),
+	`salt_string` varchar(255),
+	`isBlocked` boolean DEFAULT false,
+	`createdAt` timestamp NOT NULL DEFAULT (now()),
+	`updated_at` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	CONSTRAINT `auth_id` PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `message` (
+	`id` varchar(36) NOT NULL,
+	`sender_id` varchar(6) NOT NULL,
+	`receiver_id` varchar(6) NOT NULL,
+	`text` text NOT NULL,
+	`delivered_at` timestamp,
+	`read_at` timestamp,
+	`created_at` timestamp NOT NULL DEFAULT (now()),
+	CONSTRAINT `message_id` PRIMARY KEY(`id`)
+);
+
+ALTER TABLE `message` ADD CONSTRAINT `message_sender_id_auth_id_fk` FOREIGN KEY (`sender_id`) REFERENCES `auth`(`id`) ON DELETE no action ON UPDATE no action;
+ALTER TABLE `message` ADD CONSTRAINT `message_receiver_id_auth_id_fk` FOREIGN KEY (`receiver_id`) REFERENCES `auth`(`id`) ON DELETE no action ON UPDATE no action;
--- src/schemas/message.schema.ts
+++ src/schemas/message.schema.ts
@@ -1,12 +1,11 @@
 import { mysqlTable } from "drizzle-orm/mysql-core";
 import * as t from "drizzle-orm/mysql-core";
 import { Auth } from "../schemas/auth.schema.ts";
-import { sql } from "drizzle-orm";
-    
+
 export const message = mysqlTable(
   "message",
   {
-    id: t.varchar("id", { length: 36 }).primaryKey().generatedAlwaysAs(sql`UUID()`),
+    id: t.varchar("id", { length: 36 }).primaryKey(), // No generatedAlwaysAs(sql`UUID()`)
     senderId: t.varchar("sender_id", { length: 6 }).notNull().references(() => Auth.id),
     receiverId: t.varchar("receiver_id", { length: 6 }).notNull().references(() => Auth.id),
     text: t.text("text").notNull(),
